0a4eaf94785386c830fb7f11140660371616efa1
Samuel <sammythige26@gmail.com>
2019-04-25 10:26:08 +0300

    add multiply,divide,subtract

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,6 @@ var add = function(number1, number2) {
 return number1 + number2;
 };
 saySomething("The sum is " + add(3,5) + ".");
-var subtract = function(number1, number2) {
-return number1 - number2;
-};
-alert(subtract(10,5));
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -20,3 +16,21 @@ var add = function(number1, number2) {
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 alert(add(number1, number2));
+var subtract = function(number3, number4) {
+return number3 - number4;
+};
+var number3 = parseInt(prompt("Enter a digit:"));
+var number4 = parseInt(prompt("Enter a digit:"));
+alert(subtract(number3, number4));
+var multiply = function(number5, number6) {
+return number5 * number6;
+};
+var number5 = parseInt(prompt("Enter a digit:"));
+var number6 = parseInt(prompt("Enter a digit:"));
+alert(multiply(number5, number6));
+var divide = function(number7, number8) {
+return number7 / number8;
+};
+var number7 = parseInt(prompt("Enter a digit:"));
+var number8 = parseInt(prompt("Enter a digit:"));
+alert(divide(number7, number8));
