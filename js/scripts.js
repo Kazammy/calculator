5646dde2ab4d1aeb549d8ac88e2adc939fa72f75
Samuel <sammythige26@gmail.com>
2019-04-25 10:06:55 +0300

    initial JS

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var saySomething = function(whatToSay) {
+alert(whatToSay);
+};
+var add = function(number1, number2) {
+return number1 + number2;
+};
+saySomething("The sum is " + add(3,5) + ".");
+var subtract = function(number1, number2) {
+return number1 - number2;
+};
+alert(subtract(10,5));
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = prompt("Enter a number:");
+alert(number1);
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));
